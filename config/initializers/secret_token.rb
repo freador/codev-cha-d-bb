74cbbab47eda1981be28944ed640629910f91967
Alex Zwir <alexzwir@gmail.com>
2013-12-11 19:39:19 -0200

    Criacao projeto Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChaDeBebe::Application.config.secret_key_base = '9084fdf197c0bc8cb060ec424bc3be3455a0cbac69fa623a7f463b4674bc5aaa607d9fc0cbed83ff0f53df2da11f93c59cc185c2764a90d3cc900b19be568e63'
